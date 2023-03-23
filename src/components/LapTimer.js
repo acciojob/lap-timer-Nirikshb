88d329333c36c7dbbe812bb0a53fb5b034560527
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-23 21:03:43 +0530

    solution

--- src/components/LapTimer.js
+++ src/components/LapTimer.js
@@ -6,7 +6,7 @@ const LapTimer = () => {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
   const [laps, setLaps] = useState([]);
-  const intervalRef = useRef();
+  const intervalRef = useRef(null);
 
   useEffect(()=>{
     if(running){
@@ -14,42 +14,50 @@ const LapTimer = () => {
         setTime(prevTime => prevTime+10);
       }, 10);
     }
-    return ()=>clearInterval(intervalRef.current);
+    return () =>clearInterval(intervalRef.current);
   }, [running]);
 
-  const startTimer = ()=>setRunning(true);
-
-  const stopTimer = ()=>setRunning(false);
+  const startTimer = ()=>{
+    setRunning(true);
+  }
+  
+  const stopTimer = ()=>{
+    setRunning(false);
+  }
+  
+  const addLap = () =>{
+    setLaps([...laps, time]);
+  }
 
   const resetTimer = () =>{
     setTime(0);
-    setRunning(false);
     setLaps([]);
   }
 
-  const addLap = () =>{
-    setLaps(prev =>[...prevLaps, time]);
-  }
+  const formatTime = timeInMs => {
+    const date = new Date(timeInMs);
+    const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+    const seconds = date.getUTCSeconds().toString().padStart(2, "0");
+    const milliseconds = Math.floor(date.getUTCMilliseconds() / 10)
+      .toString()
+      .padStart(2, "0");
+    return `${minutes}:${seconds}.${milliseconds}`;
+  };
+
   return (
     <div>
-      <h1>Lap Timer</h1>
-      <div>TIme : {time} </div>
-      {!running && <button onClick={startTimer}>Start</button>}
-      {running && <button onClick={stopTimer}>Stop</button>}
-
-      <button onClick={addLap}>Lap</button>
-      <button onClick={resetTimer}>Reset</button>
-      
-        {laps.length > 0 && (
-          <div>
-            <h2>Laps</h2>
-            {laps.map((lapTime, index)=>{
-              <div key={key}>Lap {index + 1}: {lapTime}</div>
-            })}
-        </div>
-        )}
-    </div>
-  )
+    <h1>{formatTime(time)}</h1>
+    <button onClick={startTimer}>Start</button>
+    <button onClick={stopTimer}>Stop</button>
+    <button onClick={addLap}>Lap</button>
+    <button onClick={resetTimer}>Reset</button>
+    <ul>
+      {laps.map((lap, index) => (
+        <li key={index}>{formatTime(lap)}</li>
+      ))}
+    </ul>
+  </div>
+);
 }
 
 export default LapTimer;
