7654bde3ed02e07bd8804620b5ce4bbd4cb36840
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-23 20:50:05 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
+import LapTimer from './LapTimer';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <LapTimer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LapTimer.js
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+import React from "react";
+import './../styles/App.css';
+
+const LapTimer = () => {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+  const [laps, setLaps] = useState([]);
+  const intervalRef = useRef();
+
+  useEffect(()=>{
+    if(running){
+      intervalRef.current = setInterval(()=>{
+        setTime(prevTime => prevTime+10);
+      }, 10);
+    }
+    return ()=>clearInterval(intervalRef.current);
+  }, [running]);
+
+  const startTimer = ()=>setRunning(true);
+
+  const stopTimer = ()=>setRunning(false);
+
+  const resetTimer = () =>{
+    setTime(0);
+    setRunning(false);
+    setLaps([]);
+  }
+
+  const addLap = () =>{
+    setLaps(prev =>[...prevLaps, time]);
+  }
+  return (
+    <div>
+      <h1>Lap Timer</h1>
+      <div>TIme : {time} </div>
+      {!running && <button onClick={startTimer}>Start</button>}
+      {running && <button onClick={stopTimer}>Start</button>}
+
+      <button onClick={resetTimer}>Reset</button>
+      <button onClick={addLap}>Add Lap</button>
+        {laps.length > 0 && (
+          <div>
+            <h2>Laps</h2>
+            {laps.map((lapTime, index)=>{
+              <div key={key}>Lap {index + 1}: {lapTime}</div>
+            })}
+        </div>
+        )}
+    </div>
+  )
+}
+
+export default LapTimer;
